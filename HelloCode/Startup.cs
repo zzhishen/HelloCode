d1f9a7b5a51fca1787149bc5afb1e0a02141a454
Louis Leong <louis@live.com.sg>
2016-06-06 15:59:47 +0800

    initial

---
+++ HelloCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloCode.Startup))]
+namespace HelloCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
